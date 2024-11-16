525842ad4f5efc2349db999aefe849f7a6616b6e
Arthur-Jacobina <email27arthur@gmail.com>
2024-11-16 23:37:08 +0700

    fix: adjust padding and scrolling

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             <ErudaProvider>
               <MiniKitProvider>
                 <Header />
-                <body className={`${inter.className} mt-16`}>{children}</body>
+                <body className={`${inter.className} pb-20 mt-16`}>{children}</body>
                 <ToastContainer />
                 <Navigation />
               </MiniKitProvider>
