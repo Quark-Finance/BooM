63decb15d5c351e0e3272ce97f41b0cdcebb21b7
Daniel Yuki <danielyukihiga1@gmail.com>
2024-11-17 08:22:55 +0700

    fix: deploy fix v3 tailwind config bug

--- dapp/tailwind.config.ts
+++ dapp/tailwind.config.ts
@@ -10,35 +10,6 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-  		animation: {
-  			meteor: 'meteor 5s linear infinite'
-  		},
-  		keyframes: {
-  			meteor: {
-  				'0%': {
-  					transform: 'rotate(215deg) translateX(0)',
-  					opacity: '1'
-  				},
-  				'70%': {
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'rotate(215deg) translateX(-500px)',
-  					opacity: '0'
-  				},
-  				'0%': {
-  					transform: 'rotate(215deg) translateX(0)',
-  					opacity: '1'
-  				},
-  				'70%': {
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'rotate(215deg) translateX(-500px)',
-  					opacity: '0'
-  				}
-  			}
-  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
