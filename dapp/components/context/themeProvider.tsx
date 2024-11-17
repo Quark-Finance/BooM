f809b7ef88ac5c913dfd0da92ed5e9abbb5547ac
Daniel Yuki <danielyukihiga1@gmail.com>
2024-11-17 08:17:30 +0700

    fix: deploy fix v2 themeProvider misterious error

--- dapp/components/context/themeProvider.tsx
+++ dapp/components/context/themeProvider.tsx
@@ -3,26 +3,41 @@
 import setGlobalColorTheme from "@/lib/themeColors";
 import { useTheme } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import React, { createContext, useContext, useEffect, useState, } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type ThemeColors = "Zinc" | "Rose" | "Blue" | "Green" | "Orange" | "Pink" | "Purple";
+
+interface ThemeColorStateParams {
+  themeColor: ThemeColors;
+  setThemeColor: React.Dispatch<React.SetStateAction<ThemeColors>>
+}
+
+interface ThemeColorStateParams {
+  themeColor: ThemeColors;
+  setThemeColor: React.Dispatch<React.SetStateAction<ThemeColors>>;
+}
 
 const ThemeContext = createContext<ThemeColorStateParams>(
-  {} as ThemeColorStateParams,
+  {} as ThemeColorStateParams
 );
 
-export default function ThemeProvider({
-  children,
-}: ThemeProviderProps) {
+export default function ThemeProvider({ children }: ThemeProviderProps) {
   const getSavedThemeColor = () => {
     try {
       return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
     } catch (error) {
       console.error("Error getting theme color from localStorage:", error);
-      "Zinc" as ThemeColors;
+      return "Zinc" as ThemeColors; // Fixed the error here
     }
   };
 
   const [themeColor, setThemeColor] = useState<ThemeColors>(
-    getSavedThemeColor() as ThemeColors,
+    getSavedThemeColor()
   );
   const [isMounted, setIsMounted] = useState(false);
   const { theme } = useTheme();
